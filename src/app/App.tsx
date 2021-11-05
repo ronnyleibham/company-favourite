f397ad99790737963849be93add82ac17b370704
Ronny Leibham <ronnyleibham@gmx.de>
2021-11-05 16:19:58 +0100

    Implement Add Card function plus Map

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import Title from './components/Title/Title';
 import Card from './components/Card/Card';
+import Form from './components/Form/Form';
 import classes from './App.module.css';
 
 function App(): JSX.Element {
-  const [companies, _setCompanies] = useState([
+  const [companies, setCompanies] = useState([
     {
       title: 'LinkedIn',
       description: 'Lorem ipsum bal bla fiedelbumm',
@@ -19,9 +20,20 @@ function App(): JSX.Element {
     },
   ]);
 
+  function handleSubmit(todo: {
+    companyName: string;
+    companyDescription: string;
+  }) {
+    setCompanies([
+      { title: todo.companyName, description: todo.companyDescription },
+      ...companies,
+    ]);
+  }
+
   return (
     <div className={classes.App}>
       <Title></Title>
+      <Form onSubmit={handleSubmit}></Form>
       {companies.map((singleCompany) => (
         <Card
           header={singleCompany.title}
---
+++ src/app/components/Form/Form.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import classes from './Form.module.css';
+
+type FormProps = {
+  onSubmit: (todo: { companyName: string; companyDescription: string }) => void;
+};
+
+function Form({ onSubmit }: FormProps): JSX.Element {
+  const [companyName, setCompanyName] = useState('');
+  const [companyDescription, setCompanyDescription] = useState('');
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    onSubmit({ companyName, companyDescription });
+  }
+
+  return (
+    <form className={classes.form} onSubmit={handleSubmit}>
+      <label className={classes.label}>
+        Company Name:
+        <input
+          className={classes.input}
+          type="text"
+          value={companyName}
+          onChange={(event) => setCompanyName(event.target.value)}
+        />
+      </label>
+      <label className={classes.label}>
+        Company Details:
+        <textarea
+          className={classes.textarea}
+          rows={5}
+          value={companyDescription}
+          onChange={(event) => setCompanyDescription(event.target.value)}
+        ></textarea>
+      </label>
+      <label>
+        <input
+          className={classes.submit}
+          type="submit"
+          value="create new Card"
+        />
+      </label>
+    </form>
+  );
+}
+
+export default Form;
